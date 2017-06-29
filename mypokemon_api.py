6557498436b6a586e08761111caf2214f28264bf
amberoffduty <380914555@qq.com>
2017-06-29 14:48:39 +0800

    conflict fixed

--- mypokemon_api.py
+++ mypokemon_api.py
@@ -137,43 +137,6 @@ def init_api(config):
 
 	return api
 
-def init_config():
-		parser = argparse.ArgumentParser()
-		config_file = "config.json"
-
-		# If config file exists, load variables from json
-		load   = {}
-		if os.path.isfile(config_file):
-			with open(config_file) as data:
-					load.update(json.load(data))
-
-		# Read passed in Arguments
-		required = lambda x: not x in load
-		parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
-			required=required("auth_service"))
-		parser.add_argument("-u", "--username", help="Username", required=required("username"))
-		parser.add_argument("-p", "--password", help="Password")
-		parser.add_argument("-l", "--location", help="Location", required=required("location"))
-		parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
-		parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
-		parser.add_argument("-px", "--proxy", help="Specify a socks5 proxy url")
-		parser.set_defaults(DEBUG=False, TEST=False)
-		config = parser.parse_args()
-
-		# Passed in arguments shoud trump
-		for key in config.__dict__:
-			if key in load and config.__dict__[key] == None:
-					config.__dict__[key] = str(load[key])
-
-		if config.__dict__["password"] is None:
-			log.info("Secure Password Input (if there is no password prompt, use --password <pw>):")
-			config.__dict__["password"] = getpass.getpass()
-
-		if config.auth_service not in ['ptc', 'google']:
-			log.error("Invalid Auth service specified! ('ptc' or 'google')")
-			return None
-
-		return config
 
 if __name__ == "__main__":
 	config = init_config()
